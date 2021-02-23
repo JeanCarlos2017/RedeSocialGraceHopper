a1e4e6904861994f15c33e000e6d387f69100d1a
GabrielFraga962 <gabrielsouzafraga@outlook.com>
2021-02-22 23:36:44 -0300

    Upando projeto Integrador v1

---
+++ src/main/java/com/Hopper/RedeHopper/model/Tema.java
@@ -0,0 +1,38 @@
+package com.Hopper.RedeHopper.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String categoria;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	
+
+}
---
+++ src/main/java/com/Hopper/RedeHopper/repository/TemaRepository.java
@@ -0,0 +1,11 @@
+package com.Hopper.RedeHopper.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.Hopper.RedeHopper.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	List<Tema> findAllByCategoriaContainingIgnoreCase(String categoria);
+
+}
