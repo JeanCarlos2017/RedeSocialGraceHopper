aaadf813122d4b7e7b0daef94c41496936ec83bb
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-24 11:39:20 -0300

    Update TemaRepository.java
    anotação  @Repository

--- src/main/java/com/Hopper/RedeHopper/domain/repository/TemaRepository.java
+++ src/main/java/com/Hopper/RedeHopper/domain/repository/TemaRepository.java
@@ -1,10 +1,12 @@
 package com.Hopper.RedeHopper.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Hopper.RedeHopper.domain.model.TemaEntidade;
 
-public interface TemaRepository extends JpaRepository<TemaEntidade, Long>{
+@Repository
+public interface TemaRepository extends JpaRepository<TemaEntidade, Long> {
 
 	public TemaEntidade findByCategoria(char tema);
 
