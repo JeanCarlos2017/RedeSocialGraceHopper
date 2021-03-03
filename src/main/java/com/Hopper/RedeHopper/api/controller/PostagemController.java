f3902d0c6f4c228b1503665fc682b2c6c018fbfa
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-03 18:02:53 -0300

    Update PostagemController.java
    alterou as todas  e retorna uma resposta mais apropriada para os endpoints

--- src/main/java/com/Hopper/RedeHopper/api/controller/PostagemController.java
+++ src/main/java/com/Hopper/RedeHopper/api/controller/PostagemController.java
@@ -23,7 +23,7 @@ import com.Hopper.RedeHopper.domain.service.PostagemETemaService;
 import com.Hopper.RedeHopper.domain.service.PostagemService;
 
 @RestController
-@RequestMapping("/postagem")
+@RequestMapping("usuario/{id_usuario}/postagem")
 @CrossOrigin("*")
 public class PostagemController {
 	@Autowired
@@ -32,12 +32,13 @@ public class PostagemController {
 	@Autowired
 	private PostagemETemaService postTemaService;
 	
-	@PostMapping
-	public ResponseEntity<PostagemEntidade> addPostagem(@Valid @RequestBody PostagemEntidade postagem) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemService.save(postagem));
+	@PostMapping("/cadastrar")
+	public ResponseEntity<PostagemEntidade> addPostagem(@Valid @PathVariable long id_usuario,
+								@RequestBody PostagemEntidade postagem) {
+		return this.valida(postagemService.cadastraPostagem(postagem, id_usuario), HttpStatus.CREATED);
 	}
 	
-	@PutMapping("/{id_postagem}")
+	@PutMapping("/alterar/{id_postagem}")
 	public ResponseEntity<PostagemEntidade> alterarPostagem(@Valid @PathVariable long id_postagem, 
 										@RequestBody PostagemEntidade postagem) {
 		PostagemEntidade update= postagemService.put(postagem, id_postagem);
@@ -45,31 +46,28 @@ public class PostagemController {
 		else return ResponseEntity.status(HttpStatus.ACCEPTED).body(update);
 	}
 	
-	@DeleteMapping("/{id_postagem}")
+	@DeleteMapping("/deletar/{id_postagem}")
 	public ResponseEntity<Void> deletarPostagem(@PathVariable long id_postagem) {
 		boolean deletou= postagemService.delete(id_postagem);
 		if(deletou) return ResponseEntity.noContent().build();
 		else return ResponseEntity.notFound().build();
 	}
-	@GetMapping("/{id_postagem}")
+	
+	@GetMapping("/buscar/{id_postagem}")
 	public ResponseEntity<PostagemEntidade> buscarPostagemId(@PathVariable  long id_postagem) {
 		Optional<PostagemEntidade> busca= postagemService.getPostagemRepositorio().findById(id_postagem);
 		if(busca.isPresent()) return ResponseEntity.status(HttpStatus.OK).body(busca.get());
 		else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
 	
-	@GetMapping
+	@GetMapping("/listar")
 	public ResponseEntity<List<PostagemEntidade>> listarPostagem() {
 		return ResponseEntity.ok(postagemService.getPostagemRepositorio().findAll());
 	}
 	
-	@PostMapping("/{id_postagem}/add/tema/{id_tema}")
-	public ResponseEntity<Void> addTemaPostagem(@PathVariable long id_postagem, @PathVariable long id_tema){
-		boolean adicionou= postTemaService.addTemaPostagem(id_tema, id_postagem);
-		if(adicionou) return ResponseEntity.noContent().build();
-		else return ResponseEntity.notFound().build();
+	private ResponseEntity<PostagemEntidade> valida(PostagemEntidade post, HttpStatus status){
+		if (post == null) return ResponseEntity.badRequest().build();
+		else return ResponseEntity.status(status).body(post);
 	}
-	
-	
 
 }
