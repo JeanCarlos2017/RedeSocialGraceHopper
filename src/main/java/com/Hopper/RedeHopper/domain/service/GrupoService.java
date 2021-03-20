b58245b9e2cf0729eee41165c40060b8f40cac59
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-19 21:38:09 -0300

    add e listagem de grupo

--- src/main/java/com/Hopper/RedeHopper/domain/service/GrupoService.java
+++ src/main/java/com/Hopper/RedeHopper/domain/service/GrupoService.java
@@ -1,12 +1,16 @@
 package com.Hopper.RedeHopper.domain.service;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Hopper.RedeHopper.domain.model.GrupoEntidade;
-import com.Hopper.RedeHopper.domain.model.PostagemEntidade;
+import com.Hopper.RedeHopper.domain.model.TemaEntidade;
 import com.Hopper.RedeHopper.domain.model.UsuarioEntidade;
 import com.Hopper.RedeHopper.domain.repository.GrupoRepository;
 
@@ -19,10 +23,13 @@ public class GrupoService {
 	
 	@Autowired private PostagemService postagemService;
 	
-
+	@Autowired private TemaService temaService;
+	
+	
+	//valida os campos nome e descrição do grupo
 	private boolean validaGrupo(GrupoEntidade grupo) {
 //		verifico se conteudo ou a imagem foram passados e verifico se não é uma string vazia
-		if(grupo.getDescricao() != null && !grupo.getDescricao().isBlank() && !grupo.getNome().isBlank() && grupo.getNome()!= null) {
+		if(grupo.getDescricao() != null && !grupo.getDescricao().isBlank() && grupo.getNome()!= null && !grupo.getNome().isBlank()) {
 				return true; 
 			}
 		
@@ -31,6 +38,7 @@ public class GrupoService {
 		}
 	}
 	
+	//verifica se quem quer acessar o grupo é o criador dele
 	private boolean verificarDonoDoGrupo(GrupoEntidade grupoEntidade, long idUsuario) {
 		if (grupoEntidade.getCriadorGrupo().getId_usuario()== idUsuario) {
 			return true;
@@ -40,15 +48,63 @@ public class GrupoService {
 		}
 	}
 	
+	//verifica como criar grupo
 	public GrupoEntidade criarGrupo (long idUsuario, GrupoEntidade grupo) {
 		Optional<UsuarioEntidade> usuario = usuarioService.getUsuarioRepository().findById(idUsuario);
 		if (usuario.isPresent()) {
 			if (this.validaGrupo(grupo)) {
 				grupo.setCriadorGrupo(usuario.get());
+				//verifico se existe algum tema invalido - nao existente 
+				if(this.verificaTemaNaoExistente(grupo)) {
+					return null;
+				}
+				//salvo o grupo sem os temas para evitar duplicagem 
+				grupoRepositorio.save(grupo);
+				//adiciono os temas ao grupo
+				grupo = this.addTemaGrupo(grupo);
+				//salvo o grupo
 				return grupoRepositorio.save(grupo);
 			}
 		}
 		return null;
+	}
+	
+	//adiciona temas ao grupo
+	private GrupoEntidade addTemaGrupo(GrupoEntidade grupo) {
+		Set<Long> listIdTema = new HashSet<>();
+		Iterator<TemaEntidade> value = grupo.getGrupoTemaList().iterator();
+		// pega a lista com os id_tema
+		while (value.hasNext()) {
+			listIdTema.add(value.next().getId_tema());
+		}
+		// faz uma iteração sobre a lista de id_Tema e adiciona o tema no grupo
+		Iterator<Long> iteratorIdTema = listIdTema.iterator();
+		while (iteratorIdTema.hasNext()) {
+			temaService.getTemaRepositorio().findById(iteratorIdTema.next()).ifPresent(tema -> {
+				grupo.getGrupoTemaList().add(tema);
+				tema.getGrupoList().add(grupo);
+				temaService.save(tema);
+			});
+		}
+		return grupo;
+	}
+	
+	//lista os grupos criados 
+	public List<GrupoEntidade> listarGrupo(){
+		return this.grupoRepositorio.findAll();
+	}
+	
+	
+	//verifca se foi passado algum tema nao existente ao grupo
+	private boolean verificaTemaNaoExistente(GrupoEntidade grupo) {
+		Iterator<TemaEntidade> value = grupo.getGrupoTemaList().iterator();
+		while(value.hasNext()) {
+			//se encontrar algum id de uma tema inexistente retorna false e cancela o salvamento
+			if(temaService.getTemaRepositorio().findById(value.next().getId_tema()).isEmpty()) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
